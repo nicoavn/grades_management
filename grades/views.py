e9a8d8e160179e976e94c0dd791f72693bfecd08
Alvin D <nicoavn@gmail.com>
2018-10-31 12:54:03 -0400

    WIP

--- grades/views.py
+++ grades/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
+
 def login_view(request):
-    return render(request,'login.html',{})
+    return render(request, 'login.html', {})
 
 
 def grades_form(request):
-    return None
+    return render(request, 'grades_form.html', {})
 
 
 def grades_submit(request):
