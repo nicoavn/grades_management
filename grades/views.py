e38f6b175fabb190daa657e0a8f4323469e91a27
Alvin D <nicoavn@gmail.com>
2018-10-31 12:41:19 -0400

    W

--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+
+
+def grades_form(request):
+    return None
+
+
+def grades_submit(request):
+    return None
